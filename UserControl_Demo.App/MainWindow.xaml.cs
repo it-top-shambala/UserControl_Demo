1a4529664ed011473bb490ffc1ef490552133364
Starinin Andrey <starininandrey@gmail.com>
2022-09-08 17:35:56 +0300

    Init

---
+++ UserControl_Demo.App/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace UserControl_Demo.App;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private string _nameP;
+    public string NameP
+    {
+        get => _nameP;
+        set
+        {
+            if (value == _nameP) return;
+            _nameP = value;
+            OnPropertyChanged(nameof(NameP));
+        }
+    }
+
+    private int _age;
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (value == _age) return;
+            _age = value;
+            OnPropertyChanged(nameof(Age));
+        }
+    }
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ButtonReset_OnClick(object sender, RoutedEventArgs e)
+    {
+        NameP = string.Empty;
+        Age = 0;
+    }
+
+    private void ButtonSubmit_OnClick(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show($"{NameP}, {Age}");
+    }
+}
